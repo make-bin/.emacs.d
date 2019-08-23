e09301c7d4e2456e486967225766cf3d31aaf4c9
makebin <245048967@qq.com>
2019-08-23 17:58:35 +0800

    add ace-window

--- init.el
+++ init.el
@@ -34,7 +34,7 @@
 		      counsel
 		      smartparens
 	              ;; --- Themes ---
-			  monokai-theme
+			  ;;monokai-theme
 			  spacemacs-theme
 		      popwin
 		      go-mode
@@ -48,7 +48,7 @@
 			  shell-pop
 			  projectile
 			  yaml-mode
-		;;	  ace-window
+			  ace-window
 	       ;; solarized-theme
 	       ) "Default packages")
 
@@ -242,6 +242,26 @@
 ;; org
 (setq org-agenda-files '("~/org"))
 (global-set-key (kbd "C-c a") 'org-agenda)
+
+;; ace-window
+(global-set-key (kbd "M-o") 'ace-window)
+(setq aw-keys '(?1 ?2 ?3 ?4 ?5 ?6 ?7 ?8 ?9))
+(setq aw-background nil)
+(defvar aw-dispatch-alist
+  '((?x aw-delete-window "Delete Window")
+	(?m aw-swap-window "Swap Windows")
+	(?M aw-move-window "Move Window")
+	(?c aw-copy-window "Copy Window")
+	(?j aw-switch-buffer-in-window "Select Buffer")
+	(?n aw-flip-window)
+	(?u aw-switch-buffer-other-window "Switch Buffer Other Window")
+	(?c aw-split-window-fair "Split Fair Window")
+	(?v aw-split-window-vert "Split Vert Window")
+	(?b aw-split-window-horz "Split Horz Window")
+	(?o delete-other-windows "Delete Other Windows")
+	(?? aw-show-dispatch-help))
+  "List of actions for `aw-dispatch-default'.")
+
 
 
 ;;ycmd
