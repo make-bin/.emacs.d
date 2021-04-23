381880431ea2c2b7ff3cac0a13e9cbefff93ef40
马克斌 <makebin@make-bin.local>
2021-04-23 23:21:10 +0800

    set key C-c C-p to pop-tag-mark

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -4,9 +4,12 @@
 (setq gofmt-command "goimports")
 (add-hook 'before-save-hook 'gofmt-before-save)
 
+(global-set-key (kbd "C-c C-p") 'pop-tag-mark)
+
 ;; config for go-guru
 (require 'go-guru)
 (add-hook 'go-mode-hook #'go-guru-hl-identifier-mode)
+
 
 
 (provide 'init-go)
