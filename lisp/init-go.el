9ca122e2984c15e50684796cda6dd607149cf9e0
make-bin <245048967@qq.com>
2021-05-07 15:55:16 +0800

    go mod hook

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -2,22 +2,15 @@
 ;;; Commentary:
 ;; config for go-mode
 
-;;(setq gofmt-command "goimports")
-;;(add-hook 'before-save-hook 'gofmt-before-save)
-;;(global-set-key (kbd "C-c C-p") 'pop-tag-mark)
-
 (use-package go-mode
   :init
   (setq gofmt-command "goimports")
   (bind-key "C-c C-p" 'pop-tag-mark)
-  :hook (before-save-hook . gofmt-before-save))
-
-;; config for go-guru
-;;(require 'go-guru)
-;;(add-hook 'go-mode-hook #'go-guru-hl-identifier-mode)
+  :config
+  (add-hook 'before-save-hook 'gofmt-before-save))
 
 (use-package go-guru
-  :hook (go-mode . go-guru-hl-identifier-mode))
-
+  :config
+  (add-hook 'go-mode-hook #'go-guru-hl-identifier-mode))
 
 (provide 'init-go)
