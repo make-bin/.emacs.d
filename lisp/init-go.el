b3c1752f6681f381eb15ac8568f486532d0f5b93
make-bin <245048967@qq.com>
2021-05-10 11:15:10 +0800

    roll back init-go

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -2,15 +2,29 @@
 ;;; Commentary:
 ;; config for go-mode
 
-(use-package go-mode
-  :init
-  (setq gofmt-command "goimports")
-  (bind-key "C-c C-p" 'pop-tag-mark)
-  :config
-  (add-hook 'before-save-hook 'gofmt-before-save))
+;;(use-package go-mode
+;;  :init
+;;  (setq gofmt-command "goimports")
+;;  (bind-key "C-c C-p" 'pop-tag-mark)
+;;  :config
+;;  (add-hook 'before-save-hook 'gofmt-before-save))
 
-(use-package go-guru
-  :config
-  (add-hook 'go-mode-hook #'go-guru-hl-identifier-mode))
+;;(use-package go-guru
+;;  :config
+;;  (add-hook 'go-mode-hook #'go-guru-hl-identifier-mode))
+
+
+(setq gofmt-command "goimports")
+(add-hook 'before-save-hook 'gofmt-before-save)
+
+(global-set-key (kbd "C-c C-p") 'pop-tag-mark)
+
+;; config for go-guru
+(require 'go-guru)
+(add-hook 'go-mode-hook #'go-guru-hl-identifier-mode)
+
+
+
+(provide 'init-go)
 
 (provide 'init-go)
